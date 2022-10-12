19ff7d724598d16adc237bfa53e969c578660cbb
jacob <jacobbishopxy@gmail.com>
2022-10-13 07:35:29 +0800

    add `t_global_const`

---
+++ t_global_const/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#include <string>
+
+namespace constants
+{
+  inline constexpr double pi{3.14159};
+  inline constexpr double avogadro{6.0221413e23};
+  inline constexpr double myGravity{9.2};
+  inline constexpr std::string_view myName{"Avatar"};
+}
+
+#endif
---
+++ t_global_const/main.cpp
@@ -0,0 +1,19 @@
+#include "constants.h"
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+
+  std::cout << "Enter a radius: ";
+
+  int radius{};
+
+  std::cin >> radius;
+
+  std::cout << "The circumference is: " << 2.0 * radius * constants::pi << '\n';
+
+  std::cout << "The planet name is: " << constants::myName << '\n';
+
+  return 0;
+}
