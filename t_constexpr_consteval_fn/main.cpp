eb407c39f4a3a8d3501b02ec8d60ab818116b3c8
jacob <jacobbishopxy@gmail.com>
2022-10-14 16:53:42 +0800

    chore

--- t_constexpr_consteval_fn/main.cpp
+++ t_constexpr_consteval_fn/main.cpp
@@ -12,11 +12,11 @@ constexpr int greater(int x, int y)
 
 int main(int argc, char const *argv[])
 {
-  std::cout << greater(5, 6) << '\n';
-  std::cout << compileTime(greater(5, 6)) << '\n';
+  std::cout << "1. " << greater(5, 6) << '\n';
+  std::cout << "2. " << compileTime(greater(5, 6)) << '\n';
 
   int x{5};
-  std::cout << greater(x, 6) << '\n';
+  std::cout << "3. " << greater(x, 6) << '\n';
 
   return 0;
 }
