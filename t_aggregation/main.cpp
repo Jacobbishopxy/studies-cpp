17a64ca017fbf3c61242b4b33399eb1ebb745ef5
jacob <jacobbishopxy@gmail.com>
2022-11-06 21:44:32 +0800

    add 16.3

---
+++ t_aggregation/main.cpp
@@ -0,0 +1,43 @@
+
+#include <functional> // std::reference_wrapper
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Teacher {
+private:
+  std::string m_name{};
+
+public:
+  Teacher(const std::string &name) : m_name{name} {}
+
+  const std::string &getName() const { return m_name; }
+};
+
+using Teachers = std::vector<std::reference_wrapper<Teacher>>;
+
+class Department {
+private:
+  Teachers m_teachers{};
+
+public:
+  Department(const Teachers teachers) : m_teachers{teachers} {}
+};
+
+int main(int argc, char const *argv[]) {
+
+  Teacher jacob{"Jacob"};
+  Teacher april{"April"};
+
+  Teachers teachers{{jacob, april}};
+
+  {
+    //
+    Department department{teachers};
+  }
+
+  std::cout << jacob.getName() << " still exists!\n";
+  std::cout << april.getName() << " still exists!\n";
+
+  return 0;
+}
